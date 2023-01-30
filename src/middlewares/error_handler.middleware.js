a5044a998df73570e70ba6eefcb8577365d5dff0
IslamWalid <islamwalid83@gmail.com>
2023-01-30 19:35:26 +0200

    fix: respond with `status code 400` to request body syntax errors

--- src/middlewares/error_handler.middleware.js
+++ src/middlewares/error_handler.middleware.js
@@ -2,7 +2,7 @@ const log = require('fancy-log');
 
 const errorHandler = (error, req, res, next) => {
   log.error(error);
-  res.sendStatus(500);
+  res.sendStatus(error.status);
 };
 
 module.exports = errorHandler;
